9d7f527ad5b1ee707792f1ec688f6409c2e6304a
Albert <albert.kurto@gmail.com>
2024-12-18 20:15:56 +0200

    Basic Speed Tests page + static settings + base.html

--- speed_typing_website/typeapp/views.py
+++ speed_typing_website/typeapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'typeapp/index.html')
+
+def speedtests(request):
+    return render(request, 'typeapp/speedtests.html')
