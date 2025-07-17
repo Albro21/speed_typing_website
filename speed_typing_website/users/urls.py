88fff54370d9b977e74acda2eb85af140ad4d712
Albert <albert.kurto@gmail.com>
2025-07-17 14:29:30 +0100

    feat: Implement password reset functionality with email notifications and confirmation views

--- speed_typing_website/users/urls.py
+++ speed_typing_website/users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from . import views
 
 app_name = 'users'
@@ -11,4 +12,9 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('register/', views.register_view, name='register'),
     path('toggle-theme/', views.toggle_theme, name='toggle_theme'),
+    
+    path('password-reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('users:password_reset_complete')), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
